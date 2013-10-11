9e1bdca46a6448ac6988f477944b3c1aced7de8a
Xudong Zhang <xdzhang@suse.com>
2013-10-11 17:37:24 +0800

    a little fix

--- backend/vnc.pm
+++ backend/vnc.pm
@@ -24,6 +24,8 @@ sub sendkey($) {
     my $self = shift;
     my $keys = shift;
     my $keycode;
+
+    sleep 0.2;
     foreach my $key (split ("-", $keys)) {
         $keycode = $key;
         if (length($key) > 1) {
@@ -45,14 +47,64 @@ sub sendkey($) {
     }
 }
 
+sub mouse_set($$) {
+    my $self = shift;
+    my ($x, $y) = @_;
+    $self->{vnc}->mouse_move_to($x, $y);
+}
+
+sub mouse_hide(;$) {
+	my $self = shift;
+	my $border_offset = shift || 0;
+	unless($border_offset) {
+        $self->{vnc}->mouse_move_to(1024, 768);
+	}
+	else {
+		# not completely in the corner to not trigger hover actions
+        $self->{vnc}->mouse_move_to(1010, 720);
+	}
+}
+
 sub screendump() {
 	my $self = shift;
 	my ($seconds, $microseconds) = gettimeofday;
 	my $tmp = "ppm.$seconds.$microseconds.png";
-	$self->{vnc}->capture()->save($tmp);
+	$self->{screen}->capture()->save($tmp);
+    wait_for_img($tmp);
     my $img = tinycv::read($tmp);
 	unlink $tmp;
 	return $img;
+}
+
+sub wait_for_img($)
+{
+       my $tmp = shift;
+
+       my $ret;
+       while (!defined $ret) {
+         sleep(0.1);
+         my $fs = -s $tmp;
+         # if qemu did not even start writing out
+         # after 0.1s, it's most likely dead. In case
+         # this is not true on slow machines, we may
+         # need to scale this - because sleeping longer
+         # doesn't make sense
+         return unless ($fs);
+         next if ($fs < 70);
+         my $header;
+         next if (!open(PPM, $tmp));
+         if (read(PPM, $header, 70) < 70) {
+           close(PPM);
+           next;
+         }
+#         close(PPM);
+#         my ($xres,$yres) = ($header=~m/\AP6\n(?:#.*\n)?(\d+) (\d+)\n255\n/);
+#         next if(!$xres);
+#         my $d=$xres*$yres*3+length($&);
+#         next if ($fs != $d);
+	 return;
+      }
+
 }
 
 sub raw_alive($) {
@@ -68,19 +120,27 @@ sub do_start_vm {
     $ENV{VNCHOST}||="localhost:5990";
     $ENV{VNCPASSWD}||="";
     bmwqemu::diag("begin start vnc backend");
+# open two vnc connect, the "screen" is used by screendump to get screenshot    
+# the "vnc" is used to send keys or mouse move
+    for my $i (qw/screen vnc/) {
     my $vnc = Net::VNC->new({hostname => $ENV{VNCHOST}, password => $ENV{VNCPASSWD}});
     $vnc->login;
     bmwqemu::diag $vnc->name.":  ".$vnc->width."x".$vnc->height."\n";
-    $self->{vnc} = $vnc;
+    $self->{$i} = $vnc;
+    }
     $self->{'pid'} = 3000;
-    $vnc->capture()->save("vnc.png");
     sleep 3;
 }
 
 sub do_stop_vm($) {
     my $self = shift;
     $self->{vnc} = undef;
+    $self->{screen} = undef;
     printf STDERR "drop VNC connect, will auto disconnect after 15s\n";
+}
+
+sub do_loadvm($) {
+
 }
 
 1;
